a935387d70e5643992a871388852af33c87a8794
lcmortensen <lcmortensen@gmail.com>
2025-10-05 17:04:57 +1300

    Limit household solar system size to 10kW (per Part 6 of EIPC)

--- src/components/HouseholdForm/HouseholdForm.tsx
+++ src/components/HouseholdForm/HouseholdForm.tsx
@@ -900,12 +900,12 @@ const HouseholdForm: React.FC<HouseholdFormProps> = ({
                         }}
                         onBlur={(e) => {
                           const numericValue = parseFloat(e.target.value);
-                          if (numericValue >= 0 && numericValue <= 1000) {
+                          if (numericValue >= 0 && numericValue <= 10) {
                             setSolarSize(
                               isNaN(numericValue) ? 0 : numericValue,
                             );
-                          } else if (numericValue > 1000) {
-                            setSolarSize(1000);
+                          } else if (numericValue > 10) {
+                            setSolarSize(10);
                           } else {
                             setSolarSize(0);
                           }
@@ -920,12 +920,12 @@ const HouseholdForm: React.FC<HouseholdFormProps> = ({
                         onKeyDown={(e) => {
                           const currentSize = solarSize || 0;
                           if (e.key === "Enter") {
-                            if (currentSize >= 0 && currentSize <= 1000) {
+                            if (currentSize >= 0 && currentSize <= 10) {
                               setSolarSize(
                                 isNaN(currentSize) ? 0 : currentSize,
                               );
-                            } else if (currentSize > 1000) {
-                              setSolarSize(1000);
+                            } else if (currentSize > 10) {
+                              setSolarSize(10);
                             } else {
                               setSolarSize(0);
                             }
@@ -935,7 +935,7 @@ const HouseholdForm: React.FC<HouseholdFormProps> = ({
                             });
                           }
                           if (e.key === "ArrowUp") {
-                            const newValue = Math.min(currentSize + 1, 1000);
+                            const newValue = Math.min(currentSize + 1, 10);
                             setSolarSize(newValue);
                           }
                           if (e.key === "ArrowDown") {
